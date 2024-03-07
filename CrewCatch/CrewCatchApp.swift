94ec8793bedcfe54187bf30247d9e39bb16efacb
Krzysztof Kubiak <kubiak.krzysztof.99@gmail.com>
2024-03-07 21:01:37 +0100

    Initial Commit

---
+++ CrewCatch/CrewCatchApp.swift
@@ -0,0 +1,32 @@
+//
+//  CrewCatchApp.swift
+//  CrewCatch
+//
+//  Created by Krzysztof Kubiak on 07/03/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CrewCatchApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
